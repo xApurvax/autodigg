adaa0f161b6eb5404fd69f0e1775c50dfc4c78a6
xApurvax <apurva.infynno@gmail.com>
2022-09-06 11:53:28 +0530

    deploy commit

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,7 @@ import { Provider } from 'react-redux'
 import '../styles/globals.css'
 import Footer from '../Components/Footer';
 import Navbar from '../Components/Navbar';
-import store from './Redux-store/store';
-import {wrapper} from "./Redux-store/store"
+import store from '../Redux-store/store';
 
 function MyApp({ Component, pageProps }) {
   return(
